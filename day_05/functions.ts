0f60775788f185fbc3a560129a7df02a2674b365
Pedro Enciso <penciso@ucsd.edu>
2024-12-17 10:17:21 -0800

    part 5

--- day_05/functions.ts
+++ day_05/functions.ts
@@ -71,6 +71,88 @@ export function check_update_list(
   return false;
 }
 
+export function check_update_list_2(
+  updates: string[],
+  first_rule: string[],
+  second_rule: string[]
+): boolean {
+  // iterate through each list
+  for (let j = 0; j < updates.length; j++) {
+    // assert the current value is in either of the rule lists
+    const current_value = updates[j];
+    // console.log(first_rule, second_rule, current_value);
+    if (
+      first_rule.includes(current_value) ||
+      second_rule.includes(current_value)
+    ) {
+      // check if it is the last item in the array
+      if (j === updates.length - 1) {
+        // success if gotten here
+        return false;
+      } else {
+        // get an array of the next items in the list from j+1 to updates.length-1
+        const next_items = updates.slice(j + 1);
+        // get the indexes in index array of second_rule where updates[j], continue if no instances
+        const index_array = get_all_indexes(second_rule, updates[j]);
+        if (index_array.length > 0) {
+          // loop through the next items, check if any of them are equal to values of first_rule at index array
+          for (const item of next_items) {
+            const matches = index_array.filter(
+              (ind) => first_rule[ind] === item
+            );
+            // return false if there is a match
+            if (matches.length > 0) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
+export function sort_array(
+  updates: string[],
+  first_rule: string[],
+  second_rule: string[]
+): void {
+  // need to rearrange array so that it fits the rule, then take middle value
+  // loop through each item in the array, call it target
+  for (let j = 0; j < updates.length; j++) {
+    let swaps_made = true;
+    while (swaps_made) {
+      swaps_made = false;
+      // get the remaining items in the array
+      const next_items = updates.slice(j + 1);
+      // get the rule array indexes of any items
+      const index_array = get_all_indexes(second_rule, updates[j]);
+      if (index_array.length > 0) {
+        // loop through the next items, check if any of them are equal to values of first_rule at index array
+        for (const item of next_items) {
+          const matches = index_array.filter((ind) => first_rule[ind] === item);
+          // swap the numbers if there is a match and break the for of loop
+          if (matches.length > 0) {
+            // console.log(
+            //   `swapping ${updates[j]} with ${
+            //     updates[j + next_items.indexOf(item) + 1]
+            //   }`
+            // );
+            const holder = updates[j];
+            updates[j] = updates[j + next_items.indexOf(item) + 1];
+            updates[j + next_items.indexOf(item) + 1] = holder;
+            swaps_made = true;
+            break;
+          }
+        }
+      }
+    }
+
+    // compare the current with the target, if they break a rule, swap
+    // get the middle value
+  }
+}
+
 function get_all_indexes(arr: string[], val: string): number[] {
   const indexes: number[] = [];
   let i = -1;
--- day_05/index.ts
+++ day_05/index.ts
@@ -1,4 +1,9 @@
-import { check_update_list, read_data } from "./functions";
+import {
+  check_update_list,
+  check_update_list_2,
+  read_data,
+  sort_array,
+} from "./functions";
 
 async function part_one() {
   const { updates, first_rule, second_rule } = await read_data();
@@ -22,3 +27,30 @@ async function part_one() {
 }
 // correct answer is 4996
 // part_one();
+
+async function part_two() {
+  const { updates, first_rule, second_rule } = await read_data();
+  // hold all middle values
+  const middle_values: number[] = [];
+  // iterate through each update list
+  for (let i = 0; i < updates.length; i++) {
+    if (check_update_list_2(updates[i], first_rule, second_rule)) {
+      // console.log("before:", updates[i]);
+      sort_array(updates[i], first_rule, second_rule);
+      // console.log("after:", updates[i]);
+      // console.log("-----------");
+
+      let middle = updates[i][Math.round((updates[i].length - 1) / 2)];
+      middle_values.push(parseInt(middle));
+    }
+  }
+  // sum all middle values
+  const sum = middle_values.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  console.log(sum);
+}
+
+part_two();
